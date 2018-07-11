927d0d3f205b0f4759b50868dd2be113d87f708e
kaho.kobayashi <kaho.kobayashi@rakuten.com>
2018-07-11 06:11:10 +0000

    signup_login

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// user registration追加したよ、かほ
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// Login authentication 追加したよ（かほ）
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 /*Route::get('/', function () {
     return view('welcome');
@@ -20,6 +28,8 @@
 Route::get('/', function () {
     return view('welcome', ['posts' => App\Post::all(), 'tags' => App\Tag::all()]);
 });
+
+
 
 Route::post('/', function () {
     $post = new App\Post();
