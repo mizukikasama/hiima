5c09e55387e30229e29b1053dfa8374a8a8938e5
risa.morimoto <risa.morimoto@rakuten.com>
2018-07-19 06:34:34 +0000

    fukusuufinal2

--- app/Http/Requests/Formrequest.php
+++ app/Http/Requests/Formrequest.php
@@ -1,36 +1,36 @@
-<?php
+// <?php
 
-namespace App\Http\Requests;
+// namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+// use Illuminate\Foundation\Http\FormRequest;
 
-class CheckboxRequest extends FormRequest
-{
+// class CheckboxRequest extends FormRequest
+// {
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return true;
-    }
+    // public function authorize()
+    // {
+        // return true;
+    // }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
-     {  return [
-        'choice' => 'required',
-        ];
+    // public function rules()
+    //  {  return [
+        // 'choice' => 'required',
+        // ];
     
         // return [
         // 'tags'=>'required',//tags
         // 'body'=>'required|max:140',//入力内容
         // ];
-      }
+    //   }
     // public function messages(){
         // return[
             //  'tags.required'=>'タグは必須項目です。'//tags
@@ -38,6 +38,6 @@ class CheckboxRequest extends FormRequest
             //  ,'body.max'=>'記述は必須項目です。'
             //  ];
     // }
-}
+// }
 
 //多分いらないりさ
