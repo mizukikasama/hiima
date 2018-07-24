444f593936c7fd35b3b0f60e01f80453f6151a14
EC2 Default User <ec2-user@ip-172-31-41-161.us-east-2.compute.internal>
2018-07-24 01:29:03 +0000

    mzktab

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,11 +4,6 @@
 @section('content')
 <!doctype html> 
 <link rel="stylesheet" href="{{ secure_asset('css/show.css') }}">
-<p class= "pagetop">
-    <a href="#top" title="ページトップへ戻る">
-    <img src="css/hiima.logo.png"/>
-    </a>
-</p>
     <div class="row">
         <aside class="col-xs-4">
             <div class="panel panel-default">
@@ -35,11 +30,13 @@
             <ul class="nav nav-tabs nav-justified">
                 <!--ここからhistory追加-->
                 <!--足したよあき-->
-            <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">History <span class="badge">{{ $count_histories }}</span></a></li>
-               
-               
+                
+                    
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">History <span class="badge">{{ $count_histories }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
+                
+               
                 <!--<li><a href="#">Followers< /a></li>-->
                 <!--<li><a href="#">Followers< /a></li>-->
             </ul>
@@ -56,6 +53,8 @@
                 <img class="media-object img-rounded img-responsive" src="{{ asset(App\User::image_map($user->id))}}" alt="" style='width:100px;'>
       </div>
         <div style="display:inline-block; margin-top:30px;">
+            
+           
       
         <p>ユーザー名: {{App\User::find($h->user_id)->nickname}}</p> <!--追加したよ。ばなな-->
         <p>カテゴリー:  {{App\Tag::find($h->tag_id)->name}} </p>
