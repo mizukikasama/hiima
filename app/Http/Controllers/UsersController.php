80af89d6545961dc6ea37d163c6bd9843c789d71
EC2 Default User <ec2-user@ip-172-31-41-161.us-east-2.compute.internal>
2018-07-25 03:04:30 +0000

    iroirokaetayomzk

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,6 +23,7 @@ class UsersController extends Controller
     public function show($id) 
     {   
         $user = User::find($id);
+        
         // $posts = $user->posts()->orderBy('created_at', 'desc')->paginate(10); //いくつ表示させるかを変更（りな）
         $data = [
             'user' => $user,
@@ -31,8 +32,9 @@ class UsersController extends Controller
         ];
 
          $data += $this->counts($user);
-
+        
         return view('users.show', $data);
+        
     }
     
     
