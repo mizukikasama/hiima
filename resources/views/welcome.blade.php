43a006c514cbb78b22b24f31ec821e0d8db4b29c
Nanae.Yasuda <nanae.a.yasuda@rakuten.com>
2018-07-25 05:58:11 +0000

    scroll_butoon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,77 +10,77 @@
     @else
     
     <!--ばななへ　このしたは背景固定のやつ（りな）-->
-      <main>
-    <div class="cd-fixed-bg cd-bg-1">
+<main>
+      <div class="cd-fixed-bg cd-bg-1">
          <!--ばななへ　ここまでだよ（りな）-->
     
     
         <div class="text-center">
                 <p class="title-logo"><img src="image/titlelogo.png"></p>
                 <p class="hiima-kun"><a href="{{Route('signup.get')}}"><img src="image/hiima.logo.png" alt=""></a></p>
-        </div>
         
         <!--スクロールのやつ（りな）-->
-       
-      <a><span></span><span></span><span></span>Scroll</a>
-     
+            <div class = "scroll-button">   
+              <a><span></span><span></span><span></span>Learn More</a>
+            </div>
+        </div>
      <!--背景を固定したスクロールできるやつ（りな）-->
+     </div>
      
-
 <!--ばななへ　ここで閉じてるよ（りな）-->
-    </div> 
+</main>   
   
   <!--ばななへ　ここから他の画像を背景にしているよ（りな）-->
   
     <div class="cd-scrolling-bg cd-color-1">
-        <div class="cd-container">
-            <p>
-                コンテンツ
-            </p>
-        </div> 
-    </div>
+    <!--    <div class="cd-container">-->
+    <!--        <p>-->
+    <!--            コンテンツ-->
+    <!--        </p>-->
+    <!--    </div> -->
+    <!--</div>-->
     
     
  
     <div class="cd-fixed-bg cd-bg-2">
-        <h1>見出し</h1>
+       
     </div> 
   
-    <div class="cd-scrolling-bg cd-color-2">
-        <div class="cd-container">
-            <p>
-                コンテンツ
-            </p>
-        </div> 
-    </div> 
+    <!--<div class="cd-scrolling-bg cd-color-2">-->
+    <!--    <div class="cd-container">-->
+    <!--        <p>-->
+    <!--            コンテンツ-->
+    <!--        </p>-->
+    <!--    </div> -->
+    <!--</div> -->
     
     
      <div class="cd-fixed-bg cd-bg-3">
-        <h1>見出し</h1>
+       
     </div> 
   
-    <div class="cd-scrolling-bg cd-color-3">
-        <div class="cd-container">
-            <p>
-                コンテンツ
-            </p>
-        </div> 
-    </div> 
+    <!--<div class="cd-scrolling-bg cd-color-3">-->
+    <!--    <div class="cd-container">-->
+    <!--        <p>-->
+    <!--            コンテンツ-->
+    <!--        </p>-->
+    <!--    </div> -->
+    <!--</div> -->
     
-     <div class="cd-fixed-bg cd-bg-4">
-        <h1>見出し</h1>
-    </div> 
+    <!-- <div class="cd-fixed-bg cd-bg-4">-->
+        
+    <!--</div> -->
   
-    <div class="cd-scrolling-bg cd-color-4">
-        <div class="cd-container">
-            <p>
-                コンテンツ
-            </p>
-        </div> 
-    </div> 
+    <!--<div class="cd-scrolling-bg cd-color-4">-->
+    <!--    <div class="cd-container">-->
+    <!--        <p>-->
+    <!--            コンテンツ-->
+    <!--        </p>-->
+    <!--    </div> -->
+    <!--</div> -->
     
     
-</main>
+
 
 <!--ばななへ　ここまでだよ（りな）-->
 
